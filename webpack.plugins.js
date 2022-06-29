e7bc52b74d382fdb36f43b2e81fc7664c7b4234b
Sergey Popov <quolpr@gmail.com>
2022-06-29 09:54:45 +0300

    Fix https://github.com/electron-userland/electron-forge/issues/2412

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -1,3 +1,16 @@
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const relocateLoader = require("@vercel/webpack-asset-relocator-loader");
 
-module.exports = [new ForkTsCheckerWebpackPlugin()];
+module.exports = [
+  new ForkTsCheckerWebpackPlugin(),
+  {
+    apply(compiler) {
+      compiler.hooks.compilation.tap(
+        "webpack-asset-relocator-loader",
+        (compilation) => {
+          relocateLoader.initAssetCache(compilation, "native_modules");
+        }
+      );
+    },
+  },
+];
